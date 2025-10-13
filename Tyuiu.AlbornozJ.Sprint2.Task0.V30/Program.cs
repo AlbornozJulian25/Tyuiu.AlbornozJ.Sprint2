2634884053022cfe927e74b3066ca2e8957a6d50
Julián Albornoz García <Tyuiu.AlbornozJ@outlook.com>
2025-10-13 22:38:28 +0500

    Add project files.

---
+++ Tyuiu.AlbornozJ.Sprint2.Task0.V30/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.AlbornozJ.Sprint2.Task0.V30.Lib;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+DataService ds = new DataService();
+
+int x = 95;
+int y = 1735;
+bool[] res = new bool[6];
+res = ds.GetCompareOperations(x, y);
+
+Console.WriteLine("*******************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:           *");
+Console.WriteLine("*******************************");
+
+Console.WriteLine("X = " + x);
+Console.WriteLine("Y = " + y);
+
+Console.WriteLine("*******************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                 *");
+Console.WriteLine("*******************************");
+
+for (int i = 0; i < 6; i++)
+    Console.WriteLine(res[i]);
+
+Console.ReadKey();

0ca5f4dbcee3943d5360ad6a16394f5a6e96c4d7
Julián Albornoz García <Tyuiu.AlbornozJ@outlook.com>
2025-10-13 22:55:08 +0500

    Task2

---
+++ Tyuiu.AlbornozJ.Sprint2.Task2.V1/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.AlbornozJ.Sprint2.Task2.V1.Lib;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("**************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:      *");
+Console.WriteLine("**************************");
+
+Console.WriteLine("Введите значение переменной X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение переменной Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+DataService ds = new DataService();
+
+bool res = ds.CheckDotInShadedArea(x, y);
+
+Console.WriteLine("**************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:            *");
+Console.WriteLine("**************************");
+if (res)
+{
+    Console.WriteLine("Точка находиться в заштрихованной области!");
+}
+else
+{
+    Console.WriteLine("Точка не находиться в заштрихованной области");
+}
+
+Console.ReadKey();

d059c740f9606d539f52afab41b16368fff2de8b
Julián Albornoz García <Tyuiu.AlbornozJ@outlook.com>
2025-10-15 22:46:07 +0500

    Task7

---
+++ Tyuiu.AlbornozJ.Sprint2.Task7.V6.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿
+using Tyuiu.AlbornozJ.Sprint2.Task7.V6.Lib;
+
+namespace Tyuiu.AlbornozJ.Sprint2.Task7.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            double x = 0.5;
+            double y = 1.2;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AlbornozJ.Sprint2.Task7.V6/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.AlbornozJ.Sprint2.Task7.V6.Lib;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+
+Console.WriteLine("Введите значение переменной X: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение переменной Y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+bool res = ds.CheckDotInShadedArea(x, y);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+
+if (res)
+{
+    Console.WriteLine("Точка находится в заштрихованной области!");
+}
+else
+{
+    Console.WriteLine("Точка не находится в заштрихованной области");
+}
+
+Console.ReadKey();
